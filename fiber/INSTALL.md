e929aa7b70fc69880d3cb06ac6c4dc46968836af
taki0412 <taki041210@naver.com>
2022-03-08 19:39:59 +0900

    [ADD] init

---
+++ fiber/INSTALL.md
@@ -0,0 +1,14 @@
+# Installation
+Python version >= 3.7 is required.
+
+## 1. (Optional) Create a conda environment
+```
+$ conda create -n "env_name" python==3.7
+$ conda activate env_name
+```
+
+## 2. Install Requirements
+```
+$ cd WORKSPACE_ROOT
+$ pip3 install -r requirements.txt
+```
---
+++ nft/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name        = "",
+    version     = "",
+    description = "",
+    author = 'JaeHa Lee',
+    author_email = 'taki041210@naver.com',
+    install_requires = [],
+    packages = find_packages(),
+    python_requires = '>=3.7'
+)
