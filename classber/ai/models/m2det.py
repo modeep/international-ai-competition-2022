657b5dd7290ab50e4eef2c4551518f20bdd149b6
taki0412 <taki041210@naver.com>
2022-04-04 23:07:46 +0900

    [ADD] 모델 자동 설치 유틸

--- classber/ai/models/m2det.py
+++ classber/ai/models/m2det.py
@@ -3,4 +3,9 @@ from os.path import isfile
 import gdown
 
 def m2det_download():
-    if isfile("../models/m2det512_vgg.pth")
+    if isfile("../models/m2det512_vgg.pth"):
+        return
+    else:
+        url = 'https://drive.google.com/uc?id=1K6X26iAXaZDQzxQFsDFTygkQ2ZLKmjbZ'
+        out_path = '../models/m2det512_vgg.pth'
+        gdown.download(url, out_path, quiet=False)
