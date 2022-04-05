808fbb5c72ec072dfc877ec6ae63a3df3e3f7d37
taki0412 <taki041210@naver.com>
2022-04-05 13:59:18 +0900

    [ADD] 모딥컴에서 실행해보기

---
+++ classber/ai/models/m2det_model.py
@@ -0,0 +1,19 @@
+# Import Library
+from os.path import isfile
+import gdown
+
+def m2det_download():
+    if isfile("../weights/m2det512_vgg.pth"):
+        return
+    else:
+        url = 'https://drive.google.com/uc?id=1K6X26iAXaZDQzxQFsDFTygkQ2ZLKmjbZ'
+        out_path = '../weights/m2det512_vgg.pth'
+        gdown.download(url, out_path, quiet=False)
+
+def vgg_download():
+    if isfile("../weights/vgg16_reducedfc.pth"):
+        return
+    else:
+        url = 'https://drive.google.com/uc?id=1f0C0RH6DScgdiWGg3MHmNDQxbtLpb-J5'
+        out_path = '../weights/vgg16_reducedfc.pth'
+        gdown.download(url, out_path, quiet=False)
