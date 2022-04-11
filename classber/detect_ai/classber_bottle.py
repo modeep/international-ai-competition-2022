df3bf3998bde3eb32cf39b4ad92b1600399c03a1
taki0412 <taki041210@naver.com>
2022-04-11 11:32:33 +0900

    [ADD] #10 가장 큰 Object만 Return

--- classber/detect_ai/classber_bottle.py
+++ classber/detect_ai/classber_bottle.py
@@ -10,11 +10,13 @@ from utils.core import *
 
 parser = argparse.ArgumentParser(description='Classber')
 parser.add_argument('-c', '--config', default='configs/m2det512_vgg.py', type=str)
-parser.add_argument('-f', '--directory',    # default='imgs/'
-                    default=None, help='the path to demo images')
+parser.add_argument('-f', '--directory', # default=None,
+                    default='imgs/',
+                    help='the path to demo images')
 parser.add_argument('-m', '--trained_model', default='weights/m2det512_vgg.pth', type=str, help='Trained state_dict file path to open')
 parser.add_argument('--video', default=False, type=bool, help='videofile mode')
-parser.add_argument('--cam', default=0, type=int, help='camera device id')
+# parser.add_argument('--cam', default=0, type=int, help='camera device id')
+parser.add_argument('--cam', default=-1, type=int, help='camera device id')
 parser.add_argument('--show', default=True, action='store_true', help='Whether to display the images')
 args = parser.parse_args()
 
@@ -29,10 +31,10 @@ anchor_config = anchors(cfg)
 print_info('The Anchor info: \n{}'.format(anchor_config))
 priorbox = PriorBox(anchor_config)
 net = build_net('test',
-                size = cfg.model.input_size,
-                config = cfg.model.m2det_config)
+                size=cfg.model.input_size,
+                config=cfg.model.m2det_config)
 init_net(net, cfg, args.trained_model)
-print_info('===> Finished constructing and loading model',['yellow','bold'])
+print_info('===> Finished constructing and loading model', ['yellow', 'bold'])
 net.eval()
 with torch.no_grad():
     priors = priorbox.forward()
@@ -61,32 +63,48 @@ cats = [_.strip().split(',')[-1] for _ in open('data/coco_labels.txt', 'r').read
 labels = tuple(['__background__'] + cats)
 
 
-def draw_detection(im, bboxes, scores, cls_inds, fps, thr=0.2):
+def draw_detection(im, bboxes, scores, cls_inds, fps, thr=0.8):
     imgcv = np.copy(im)
     h, w, _ = imgcv.shape
+    boxs = []
+    boxs_area = []
+    boxs_score = []
     for i, box in enumerate(bboxes):
         cls_indx = int(cls_inds[i])
         if cls_indx == 40 or cls_indx == 42:
             if scores[i] < thr:
                 continue
-            box = [int(_) for _ in box]
-            thick = int((h + w) / 300)
-            cv2.rectangle(imgcv,
-                          (box[0], box[1]), (box[2], box[3]),
-                          colors[cls_indx], thick)
-            mess = '%s: %.3f' % (labels[cls_indx], scores[i])
-            cv2.putText(imgcv, mess, (box[0], box[1] - 7),
-                        0, 1e-3 * h, colors[cls_indx], thick // 3)
-            if fps >= 0:
-                cv2.putText(imgcv, '%.2f' % fps + ' fps', (w - 160, h - 15), 0, 2e-3 * h, (255, 255, 255), thick // 2)
+            else:
+                boxs.append([box[0], box[1], box[2], box[3]])
+                boxs_area.append(((box[2] - box[0]) * (box[3] - box[1])))
+                boxs_score.append(scores[i])
         else:
-            pass
-    return imgcv
+            continue
+
+    thick = int((h + w) / 300)
+    if fps >= 0:
+        cv2.putText(imgcv, '%.2f' % fps + ' fps', (w - 160, h - 15), 0, 2e-3 * h, (255, 255, 255), thick // 2)
+
+    if len(boxs_area) == 0:
+        return imgcv, None
+    else:
+        n = boxs_area.index(max(boxs_area))
+        cv2.rectangle(imgcv,
+                      (int(boxs[n][0]), int(boxs[n][1])), (int(boxs[n][2]), int(boxs[n][3])),
+                      (0, 0, 255), thick)
+        mess = 'bottle: %.3f' % (boxs_score[n])
+        cv2.putText(imgcv, mess, (int(boxs[n][0]), int(boxs[n][1]) - 7),
+                    0, 1e-3 * h, (0, 0, 255), thick // 3)
+        return imgcv, boxs[n]
 
 
 im_path = args.directory
 cam = args.cam
 video = args.video
+
+im_fnames = sorted((fname for fname in os.listdir(im_path) if os.path.splitext(fname)[-1] == '.jpg'))
+im_fnames = (os.path.join(im_path, fname) for fname in im_fnames)
+im_iter = iter(im_fnames)
 
 if cam >= 0:
     capture = cv2.VideoCapture(cam)
@@ -105,9 +123,12 @@ if cam >= 0 or video:
     video_name = os.path.splitext(video_path)
     fourcc = cv2.VideoWriter_fourcc('m','p','4','v')
     out_video = cv2.VideoWriter(video_name[0] + '_m2det.mp4', fourcc, capture.get(cv2.CAP_PROP_FPS), (int(capture.get(cv2.CAP_PROP_FRAME_WIDTH)), int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))))
-im_fnames = sorted((fname for fname in os.listdir(im_path) if os.path.splitext(fname)[-1] == '.jpg'))
-im_fnames = (os.path.join(im_path, fname) for fname in im_fnames)
-im_iter = iter(im_fnames)
+
+
+def location_pass(location):
+    print(location)
+    return location
+
 
 while True:
     if cam < 0 and not video:
@@ -126,7 +147,7 @@ while True:
             break
 
     loop_start = time.time()
-    w, h = image.shape[1],image.shape[0]
+    w, h = image.shape[1], image.shape[0]
     img = _preprocess(image).unsqueeze(0)
     if cfg.test_cfg.cuda:
         img = img.cuda()
@@ -157,17 +178,16 @@ while True:
     cls_list = cls_inds.tolist()
     cls_list = list(map(int, cls_list))
 
-    # @문서준 이거 보세요.
-    print('\n'.join(['pos:{}, ids:{}, score:{:.3f}'.format('(%.1f,%.1f,%.1f,%.1f)' % (o[0], o[1], o[2], o[3]),
-                                               labels[int(oo)], ooo) for o, oo, ooo in zip(boxes, cls_inds, scores)]))
+    # print('\n'.join(['pos:{}, ids:{}, score:{:.3f}'.format('(%.1f,%.1f,%.1f,%.1f)' % (o[0], o[1], o[2], o[3]),
+    #                                    labels[int(oo)], ooo) for o, oo, ooo in zip(boxes, cls_inds, scores)]))
 
     fps = 1.0 / float(loop_time) if cam >= 0 or video else -1
     for i in cls_list:
         if i == 40 or i == 42:
-            im2show = draw_detection(image, boxes, scores, cls_inds, fps)
+            im2show, img_location = draw_detection(image, boxes, scores, cls_inds, fps)
         # print bbox_pred.shape, iou_pred.shape, prob_pred.shape
         else:
-            pass
+            continue
     # print bbox_pred.shape, iou_pred.shape, prob_pred.shape
 
     if im2show.shape[0] > 1100:
@@ -184,7 +204,9 @@ while True:
                 out_video.release()
                 capture.release()
                 break
+
     if cam < 0 and not video:
         cv2.imwrite('{}_m2det.jpg'.format(fname.split('.')[0]), im2show)
     else:
         out_video.write(im2show)
+    location_pass(img_location)
